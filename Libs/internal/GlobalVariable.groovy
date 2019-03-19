cf3dbf7a5adf6b6591d8679f0f39dc9ba352578b
Brian Ducson <brian.ducson@gmail.com>
2019-03-19 14:30:08 +0700

    update some tests

--- Libs/internal/GlobalVariable.groovy
+++ Libs/internal/GlobalVariable.groovy
@@ -1,12 +1,8 @@
 package internal
 
 import com.kms.katalon.core.configuration.RunConfiguration
-import com.kms.katalon.core.testobject.ObjectRepository as ObjectRepository
-import com.kms.katalon.core.testdata.TestDataFactory as TestDataFactory
-import com.kms.katalon.core.testcase.TestCaseFactory as TestCaseFactory
-import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
-import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
-import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import com.kms.katalon.core.main.TestCaseMain
+
 
 /**
  * This class is generated automatically by Katalon Studio and should not be modified or deleted.
@@ -35,23 +31,18 @@ public class GlobalVariable {
      
 
     static {
-        def allVariables = [:]        
-        allVariables.put('default', ['authorization' : 'Basic ZGVtb0BrYXRhbG9uLmNvbTpWQm5jZXRPU083MEU0TlZMWVFuaDlCMkQ=', 'issue_key' : 'KD-1000', 'update_issue_key' : 'KD-1001', 'latest_response' : ''])
-        
-        String profileName = RunConfiguration.getExecutionProfile()
-        def selectedVariables = allVariables[profileName]
-		
-		for(object in selectedVariables){
-			String overridingGlobalVariable = RunConfiguration.getOverridingGlobalVariable(object.key)
-			if(overridingGlobalVariable != null){
-				selectedVariables.put(object.key, overridingGlobalVariable)
-			}
-		}
-
-        authorization = selectedVariables["authorization"]
-        issue_key = selectedVariables["issue_key"]
-        update_issue_key = selectedVariables["update_issue_key"]
-        latest_response = selectedVariables["latest_response"]
-        
+        try {
+            def selectedVariables = TestCaseMain.getGlobalVariables("default")
+			selectedVariables += TestCaseMain.getGlobalVariables(RunConfiguration.getExecutionProfile())
+            selectedVariables += RunConfiguration.getOverridingParameters()
+    
+            authorization = selectedVariables['authorization']
+            issue_key = selectedVariables['issue_key']
+            update_issue_key = selectedVariables['update_issue_key']
+            latest_response = selectedVariables['latest_response']
+            
+        } catch (Exception e) {
+            TestCaseMain.logGlobalVariableError(e)
+        }
     }
 }
